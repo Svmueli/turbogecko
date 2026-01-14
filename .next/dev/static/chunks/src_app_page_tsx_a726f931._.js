879562721e1a94f44083b7ad87b831db88eefab4
Sammy Kioko <pt_svmueli@localhost.localdomain>
2026-01-14 13:41:44 +0300

    fix: ignore heavy folders

--- .next/dev/static/chunks/src_app_page_tsx_a726f931._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_74a6d215._.js",
-  "static/chunks/src_c7f72f1d._.js"
-],
-    source: "dynamic"
-});
